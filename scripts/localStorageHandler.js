6de8b908577dccbb4a60f7864b35d4bba270e088
Immo <seiryu@derg.immo>
2020-05-15 12:26:38 +0200

    Added localStorage handler

---
+++ scripts/localStorageHandler.js
@@ -0,0 +1,45 @@
+/**
+ * This is a handler for localStorage.
+ */
+
+/**
+ * 
+ * @param {String} value The value to get in localStorage
+ * @returns {String}
+ */
+function getStorage(value) {
+	if (!window) throw ReferenceError("This has to be run in a browser.");
+	return window.localStorage.getItem(value);
+}
+
+/**
+ * 
+ * @param {String} value The value to get in localStorage
+ * @returns {Number}
+ */
+function getStorageAsNumber(value) {
+	if (!window) throw ReferenceError("This has to be run in a browser.");
+	const _value = window.localStorage.getItem(value);
+	if (!isNaN(_value)) return parseInt(_value);
+	else return NaN;
+}
+/**
+ * 
+ * @param {String} name 
+ * @param {any} value 
+ * @param {Boolean} overwrite 
+ */
+function addStorage(name, value, overwrite = false) {
+	if (!window) throw ReferenceError("This has to be run in a browser.");
+	const array = [];
+	if (typeof value == "Number") value = value.toString();
+	if (overwrite) {
+		for (const c of JSON.parse(window.localStorage.getItem(name))) {
+			array.push(c);
+		}
+		array.push(value);
+		window.localStorage.setItem(name, JSON.stringify(array));
+	} else {
+		window.localStorage.setItem(name, [value]);
+	}
+}
