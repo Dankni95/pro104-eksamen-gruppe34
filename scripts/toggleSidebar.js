6e39ef9af37d4fae460ef2e4f3dc03c956d501bf
ReHaugen <Reidar_Haugen"outlook.com>
2020-05-21 19:00:58 +0200

    Update toggleSidebar.js
    Expand Script

--- scripts/toggleSidebar.js
+++ scripts/toggleSidebar.js
@@ -3,4 +3,12 @@ function toggleSidebar(){
     document.getElementById("main_container").classList.toggle('active');
     document.getElementById("miniContainer").classList.toggle('active');
     document.getElementById("miniAdd").classList.toggle('active');
+
+}
+
+function expandTask(){
+    document.getElementById("taskTab").classList.toggle('active');
+    document.getElementById("miniChart1").classList.toggle('active');
+    
+   
 }
