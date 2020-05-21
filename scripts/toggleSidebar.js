110f5ad390415f8ed5b51440585e81a3fd7da0ac
ReHaugen <Reidar_Haugen@outlook.com>
2020-05-21 13:10:34 +0200

    Create toggleSidebar.js
    Added sidebar script til script folder

---
+++ scripts/toggleSidebar.js
@@ -0,0 +1,6 @@
+function toggleSidebar(){
+    document.getElementById("left_container").classList.toggle('active');
+    document.getElementById("main_container").classList.toggle('active');
+    document.getElementById("miniContainer").classList.toggle('active');
+    document.getElementById("miniAdd").classList.toggle('active');
+}
