866fa54e33d8bef84303c2c9bf6dfd0cf34dad0f
ReHaugen <Reidar_Haugen@outlook.com>
2020-05-21 13:09:18 +0200

    Create pieChart.js
    Piechart script

---
+++ scripts/pieChart.js
@@ -0,0 +1,26 @@
+google.charts.load('current', {'packages' :['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+    
+function drawChart(){
+
+    var data = google.visualization.arrayToDataTable([
+        ['Task', 'Completed'],
+        ['nr1', 11],
+        ['nr2', 8],
+        ['nr3', 5]
+
+    
+    ]);
+
+    var options = {
+        title: 'Task Overview',
+        backgroundColor: "none",
+        chartArea: {left: "40"},
+       
+
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById("piechart"));
+    
+    chart.draw(data, options);
+}
