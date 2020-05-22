828a747cb2158bdef0f8b59eed8ea5bbe84ed8a9
ChrisBergland <54422122+ChrisBergland@users.noreply.github.com>
2020-05-22 13:10:05 +0200

    Drag and drop from person list to taskbar

--- scripts/toggleSidebar.js
+++ scripts/toggleSidebar.js
@@ -2,13 +2,23 @@ function toggleSidebar(){
     document.getElementById("left_container").classList.toggle('active');
     document.getElementById("main_container").classList.toggle('active');
     document.getElementById("miniContainer").classList.toggle('active');
-    document.getElementById("miniAdd").classList.toggle('active');
-
+    document.getElementById("miniAdd").classList.toggle('active');;
 }
 
 function expandTask(){
     document.getElementById("taskTab").classList.toggle('active');
-    document.getElementById("miniChart1").classList.toggle('active');
-    
-   
+    document.getElementById("miniChart1").classList.toggle('active'); 
+    document.getElementById("dropPerson1").classList.toggle('active');
+}
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+ev.dataTransfer.setData("text", ev.target.id);
+}
+function drop(ev) {
+ev.preventDefault();
+var data = ev.dataTransfer.getData("text");
+ev.target.appendChild(document.getElementById(data));
 }
