86e7c473d890a93ce0ec613d24dafe184aec1f4e
ChrisBergland <54422122+ChrisBergland@users.noreply.github.com>
2020-05-24 12:14:04 +0200

    Copy person

--- scripts/toggleSidebar.js
+++ scripts/toggleSidebar.js
@@ -23,5 +23,7 @@ ev.dataTransfer.setData("text", ev.target.id);
 function drop(ev) {
 ev.preventDefault();
 var data = ev.dataTransfer.getData("text");
-ev.target.appendChild(document.getElementById(data));
+var nodeCopy = document.getElementById(data).cloneNode(true);
+nodeCopy.id = "newId";
+ev.target.appendChild(nodeCopy);
 }
